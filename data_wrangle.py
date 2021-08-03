5794621f099002cc398930ef4eb8786d7f6c8cd0
mabhutidlodlo <mabhutidlodlo1@gmail.com>
2021-08-03 12:42:26 +0200

    complete project

---
+++ data_wrangle.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+data_url ='https://en.wikipedia.org/wiki/Road_safety_in_Europe'
+
+page = requests.get(data_url)
+table_class="wikitable sortable jquery-tablesorter"
+
+if page.status_code ==200:
+
+  soup = BeautifulSoup(page.text, 'html.parser')
+  table=soup.find('table',{'class':"wikitable sortable"})
+  df=pd.read_html(str(table))
+  df=pd.DataFrame(df[0])
+  A_sorted = df.sort_values("Total Road Deaths in 2018[30]")
+  df.to_csv('data.csv')
